e654bd8eadef6b246d3792ea8212f5e5355308f3
Jennifer Hu <hujj@mit.edu>
2014-04-16 19:31:41 -0400

    javascript move to file and play/pause video on click

--- js/reviewer.js
+++ js/reviewer.js
@@ -1,36 +1,61 @@
-$(document).ready(function(){	
+$(document).ready(function() {
 	var video=document.getElementById("video"); 
 	$('#cmtbtn').click(function(){
-	  		// $('#message_list').append("hello");
-	  		$('#comment_list').append($('#comment').val()+ '<br>');
+	  		// $('#comment_list').append($('#comment').val()+ '<br>');
+	  		$('#comment_list').append('<div id="individual_comment"><table border="1" id="cmt_table"><th id="time_stamp">'+$('#comment').val().substring(1,5)+
+									'</th><th id="comment_text">'+$('#comment').val().substring(6)+'</div>')
 	  		$('#comment').val('');
 	  		video.play();
 	  	});
 
-	$("#comment").keyup(function(event){ 
+
+	$('#changevote1').keyup(function(){
 		if(event.keyCode == 13){
-			$("#cmtbtn").click();
+			$("#yeschange").click();
 		}
 	});
 
-	$('input[type="text"]').focus(function() {
-		video.pause();
+	$('#changevote2').keyup(function(){
+		if(event.keyCode == 13){
+			$("#yeschange2").click();
+		}
+	});
+
+	$("#comment").keydown(function(event){ 
+		if(event.keyCode == 13){
+			$("#cmtbtn").click();
+		}
+		else if (!video.paused){
+			video.pause();
+		}
+		console.log("get here?")
+
 		var minutes = Math.floor(video.currentTime/60);
 		var seconds = Math.floor(video.currentTime-minutes*60)
 		if (seconds<10){
 			seconds='0'+seconds
 		}
-
-		$('#comment').val('['+minutes+":"+seconds+']  ');
+		if ($('#comment').val().length==1){
+			console.log("get here?now")
+			$('#comment').val('['+minutes+":"+seconds+'] '+$('#comment').val());
+		}
 
 	});
 
 
+
 	$(window).click(function(e) {
 		if ((42<=event.clientX<=660) && (126<=event.clientY<=200)){
-			console.log("HELLOO")
-			console.log(event.clientX)
-			console.log(event.clientY)
+			// console.log("HELLOO")
+			// console.log(event.clientX)
+			// console.log(event.clientY)
+			// if (video.paused){
+			// 	video.play();
+			// }
+			// else{
+			// 	video.pause();
+			// }
+
 		}
 	});
 
@@ -45,39 +70,83 @@ $(document).ready(function(){
 		$('#notsolddisplay').removeClass('hidden')
 	});
 
+
 	$('#changebtn').click(function(){
 
-
-		$('#confirm').removeClass("hidden");
+		$('#confirmfromsold').removeClass("hidden");
 		$('#solddisplay').addClass('hidden');
+		$('#changevote1').focus()
 
 		$('#yeschange').click(function(){
-			$('#confirm').addClass("hidden");
-			$('#notsolddisplay').removeClass('hidden');
-		});
+			if ($("#changevote1").val().length===0){
+				console.log("lengthbelow")
+				console.log($("#changevote1").val().length);
+				alert("You must provide a reason for changing")
+			}	
+			else{
+				console.log("alert?")
+				console.log($("#changevote1").val().length)
+				console.log($("#changevote1").val())
+				$('#confirmfromsold').addClass("hidden");
+				$('#notsolddisplay').removeClass('hidden');
+			}
+		})
 		$('#nochange').click(function(){
-			$('#confirm').addClass("hidden");
+			$('#confirmfromsold').addClass("hidden");
 			$('#solddisplay').removeClass('hidden');
-		});
+		})
 	});
 
 	$('#changebtn2').click(function(){
 
-
-		$('#confirm').removeClass("hidden");
+		$('#confirmfromnotsold').removeClass("hidden");
 		$('#notsolddisplay').addClass('hidden');
+		$('#changevote2').focus()
 
-		$('#yeschange').click(function(){
-			$('#confirm').addClass("hidden");
-			$('#solddisplay').removeClass('hidden');
+		$('#yeschange2').click(function(){
+			if ($("#changevote2").val().length==0){
+				alert("You must provide a reason for changing")
+			}
+			else{
+				$('#confirmfromnotsold').addClass("hidden");
+				$('#solddisplay').removeClass('hidden');
+			}
 		});
-		$('#nochange').click(function(){
-			$('#confirm').addClass("hidden");
+		$('#nochange2').click(function(){
+			$('#confirmfromnotsold').addClass("hidden");
 			$('#notsolddisplay').removeClass('hidden');
 		});
 
 	});
 
+	/*COMMENTS AREA*/
+
+	$('#hidecomments').click(function(){
+		$('#hidecomments').addClass('hidden');
+		$('#comments').addClass('hidden');
+		$('#showcomments').removeClass('hidden');
+	});
+
+	$('#showcomments').click(function(){
+		$('#comments').removeClass('hidden');
+		$('#hidecomments').removeClass('hidden');
+		$('#showcomments').addClass('hidden');
+	});
+
+	function togglePlay() {
+	    if (video.paused == true) {
+	    // Play the video
+	    video.play();
+	  } else {
+	    // Pause the video
+	    video.pause();
+	  }
+	}
 
 
-})
+	video.addEventListener("click", function() {
+    	togglePlay();
+  	});
+
+
+});
