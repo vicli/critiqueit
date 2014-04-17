f47de2542be2f23f5d68ecc43b2fb177962c9ed0
Kristin Au <kristincau@Kristins-MacBook-Pro.local>
2014-04-16 21:43:31 -0400

    newest updates

--- js/control_bar.js
+++ js/control_bar.js
@@ -22,6 +22,8 @@ window.onload = function() {
   var seekBar = document.getElementById("seek-bar");
   var volumeBar = document.getElementById("volume-bar");
 
+  var comment_divs =[];
+
   function togglePlay() {
       if (video.paused == true) {
       // Play the video
@@ -43,7 +45,7 @@ window.onload = function() {
       var bubble = document.createElement('img');
       bubble.className = 'comment_img';
       bubble.src = 'images/comment.png';
-      bubble.id = j; 
+      bubble.id = '_'+j; 
       bubble.style.marginLeft = 800/totalJumps-15+'px';
       parent.appendChild(bubble);
     }
@@ -72,12 +74,34 @@ fullScreenButton.addEventListener("click", function() {
   }
 });
 
-seekBar.addEventListener("change", function() {
-  // Calculate the new time
-  var time = video.duration * (seekBar.value / 100);
+ seekBar.addEventListener("change", function() {
+ // Calculate the new time
+ var time = video.duration * (seekBar.value / 100);
 
-  // Update the video time
-  video.currentTime = time;
+ // Update the video time
+ video.currentTime = time;
+
+
+
+ var value = (100 / video.duration) * video.currentTime;
+ seekBar.value = value;
+ $("#current_time").html(getVideoTime(video.currentTime));
+
+ var total_jump = Math.floor(video.duration)/10;
+ var single_jump = 800/total_jump;
+ var current_jump = Math.floor(video.currentTime)/10;
+ var jump_value = single_jump*current_jump;
+
+ var stamp = Math.floor(current_jump)*10;
+
+ $('#ind_comment_container').html('');
+ $('#pointer').css({"marginLeft": jump_value+'px'});
+ for (var n=0; n< fake_comments.length; n++) {
+   if (fake_comments[n].getTimeStamp() > stamp && fake_comments[n].getTimeStamp() < stamp+10) {
+     fake_comments[n].render_stamp();
+   } 
+ }
+
 });
 
 seekBar.addEventListener("mousedown", function() {
@@ -97,18 +121,42 @@ video.addEventListener("timeupdate", function() {
   var total_jump = Math.floor(video.duration)/10;
   var single_jump = 800/total_jump;
   var current_jump = Math.floor(video.currentTime)/10;
+  var last_jump = current_jump-1;
   var jump_value = single_jump*current_jump;
 
   var stamp = Math.floor(current_jump)*10;
 
+
   if (Math.floor(video.currentTime)%10 == 0) {
     $('#ind_comment_container').html('');
+    $('#'+current_jump).animate({'width' : '25px', 
+                                  'height': '22px'}, 200);
+    $('#'+last_jump).animate({'width' : '20px', 
+                                  'height': '17px'}, 200);
+    $('#start_time').html(getVideoTime(stamp));
+    $('#end_time').html(getVideoTime(stamp+10));
+
     $('#pointer').css({"marginLeft": jump_value+'px'});
     for (var n=0; n< fake_comments.length; n++) {
       if (fake_comments[n].getTimeStamp() > stamp && fake_comments[n].getTimeStamp() < stamp+10) {
         fake_comments[n].render_stamp();
       } 
     }
+    var div_time = document.getElementsByClassName('individual_comment');
+    for (var d=0;d<div_time.length;d++) {
+      var time = div_time[d].id;
+      document.getElementById(time).addEventListener("click", function(e) {
+
+        var value = (100 / video.duration) * e.target.id;
+        seekBar.value = value;
+        var time = video.duration * (seekBar.value / 100);
+         video.currentTime = time;
+         video.play();
+        playButton.src = 'images/pause.png';
+
+      });
+    }
+
 
   }
 });
